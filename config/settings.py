debb4c6cec52ee6dfa3ce83d26ae4cf840d1b5d1
Tomas Martinez Valdivia <tom.martinezv@duocuc.cl>
2026-05-12 18:10:36 -0400

    Permitir cors en bff para el front

--- config/settings.py
+++ config/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_web_api',
+    'corsheaders',  # Para manejar CORS
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
